11f660a03029bf17812d3d852d4ab10d3c5155e1
b10s <and.enshin@gmail.com>
2014-12-16 16:52:27 +0300

    fixed few bugs

--- parse.pl
+++ parse.pl
@@ -2,7 +2,9 @@
 
 use Data::Dumper;
 
-$accept_langs="ru, en-gb;q=0.8, en;q=0.7";
+$accept_langs="en-US,en;q=0.8";
 
+my @accept_langs = map { s/^\s+|\s+$//g; /([\w-]+)(;q=(\d+(\.\d+)?))?/i; {l=>$1, q=>$3//1} } split(",", $accept_langs);
+sort { $b->{q} <=> $a->{q} } @accept_langs;
 
-print Dumper(sort { $b->{q} <=> $a->{q} } map { s/^\s+|\s+$//g; /([\w-]+)(;q=(\d+(\.\d+)?))/; {l=>$1, q=>$3//1} } split(",", $accept_langs) );
+print Dumper(@accept_langs);
